19a9fd6924469a0428a28d1b0d58f2d0ba6c7952
Joshua Ashton <joshua@froggi.es>
2023-10-25 22:55:29 +0100

    Log more things

--- main.c
+++ main.c
@@ -12,8 +12,15 @@
 
 #define MURA_BLOB_SIZE (2048 * 1024)
 
+#define PANEL_UNIQUE_ID_BASE (0x0360)
+#define PANEL_TDM            (0x0370)
+
 struct buffer {
-    char *data;
+    union {
+        char *s8;
+        unsigned char *u8;
+    } data;
+
     size_t size;
 };
 
@@ -26,20 +33,20 @@ static struct buffer read_file(const char *path, const char *mode, off_t offset,
     FILE *file = fopen(path, mode);
     if (!file) {
         fprintf(stderr, "Failed to open %s: %s\n", path, strerror(errno));
-        return (struct buffer) { NULL, 0 };
+        return (struct buffer) { { NULL }, 0 };
     }
 
     if (!size) {
         if (fseeko(file, 0, SEEK_END) != 0) {
             fprintf(stderr, "Failed to seek in %s: %s\n", path, strerror(errno));
-            return (struct buffer) { NULL, 0 };
+            return (struct buffer) { { NULL }, 0 };
         }
 
         off_t fsize = ftello(file);
         off_t remaining_size = fsize - offset;
         if (remaining_size <= 0) {
             fprintf(stderr, "Tried to seek to an invalid offset in %s.\n", path);
-            return (struct buffer) { NULL, 0 };
+            return (struct buffer) { { NULL }, 0 };
         }
 
         size = (size_t)remaining_size;
@@ -47,26 +54,26 @@ static struct buffer read_file(const char *path, const char *mode, off_t offset,
 
     if (fseeko(file, offset, SEEK_SET) != 0) {
         fprintf(stderr, "Failed to seek in %s: %s\n", path, strerror(errno));
-        return (struct buffer) { NULL, 0 };
+        return (struct buffer) { { NULL }, 0 };
     }
 
-    struct buffer buf = { (char *)malloc(size + 1), size };
-    if ((size = fread(buf.data, 1, buf.size, file)) <= 0) {
+    struct buffer buf = { { (char *)malloc(size + 1) }, size };
+    if ((size = fread(buf.data.s8, 1, buf.size, file)) <= 0) {
         if (!feof(file)) {
             if (ferror(file))
                 fprintf(stderr, "Error reading %s: %s\n", path, strerror(errno));
             else
                 fprintf(stderr, "Error reading %s: Unknown error\n", path);
-            return (struct buffer) { NULL, 0 };
+            return (struct buffer) { { NULL }, 0 };
         }
     }
     buf.size = size;
     /* Always null terminate incase we treat this as a string at some point */
-    buf.data[size] = '\0';
+    buf.data.s8[size] = '\0';
 
     if (fclose(file) != 0) {
         fprintf(stderr, "Failed to close file %s: %s\n", path, strerror(errno));
-        return (struct buffer) { NULL, 0 };
+        return (struct buffer) { { NULL }, 0 };
     }
 
     return buf;
@@ -79,7 +86,7 @@ static int write_file(const char *path, const char *mode, struct buffer data) {
         return 1;
     }
 
-    if (fwrite(data.data, 1, data.size, file) != data.size) {
+    if (fwrite(data.data.s8, 1, data.size, file) != data.size) {
         fprintf(stderr, "Failed to write to file %s: %s\n", path, strerror(errno));
         return 1;
     }
@@ -121,25 +128,57 @@ int main(int argc, char* argv[]) {
     if (!product.size)
         return 1;
 
-    if (strncmp(vendor.data, "Valve\n", vendor.size)) {
-        fprintf(stderr, "Vendor didn't match. Was: %.*s Expected: %s\n", (int)vendor.size, vendor.data, "Valve\n");
+    if (strncmp(vendor.data.s8, "Valve\n", vendor.size)) {
+        fprintf(stderr, "Vendor didn't match. Was: %.*s Expected: %s\n", (int)vendor.size, vendor.data.s8, "Valve\n");
         return 1;
     }
 
-    if (strncmp(product.data, "Galileo\n", product.size)) {
-        fprintf(stderr, "Product didn't match. Was: %.*s Expected: %s\n", (int)product.size, product.data, "Galileo\n");
+    if (strncmp(product.data.s8, "Galileo\n", product.size)) {
+        fprintf(stderr, "Product didn't match. Was: %.*s Expected: %s\n", (int)product.size, product.data.s8, "Galileo\n");
+        return 1;
+    }
+
+    /* Grab display serial from dp aux. */
+    struct buffer display_serial = read_file("/dev/drm_dp_aux0", "rb", PANEL_UNIQUE_ID_BASE, 12);
+    if (display_serial.size < 12) {
+        fprintf(stderr, "Failed to get display serial.\n");
         return 1;
     }
 
     /*
-     * Grab display serial from dp aux.
-     * We only do mura correction on SDC screens, which have a serial of length 5.
+     * SDC serials are only 5 characters long, HEX coded.
+     * BOE serials start with SED and are 12 characters, ASCII.
      */
-    struct buffer display_serial = read_file("/dev/drm_dp_aux0", "rb", 864, 5);
-    if (display_serial.size < 5) {
-        fprintf(stderr, "Failed to get display serial.\n");
+    bool is_boe = display_serial.data.s8[0] == 'S'
+               && display_serial.data.s8[1] == 'E'
+               && display_serial.data.s8[2] == 'D'
+               && (display_serial.data.s8[5]  != '\0' || display_serial.data.s8[6]  != '\0'
+                || display_serial.data.s8[7]  != '\0' || display_serial.data.s8[8]  != '\0'
+                || display_serial.data.s8[9]  != '\0' || display_serial.data.s8[10] != '\0'
+                || display_serial.data.s8[11] != '\0');
+
+    int is_antiglare = -1;
+    if (is_boe)
+        is_antiglare = display_serial.data.s8[3] == 'S' ? 1 : 0;
+
+#ifdef DEBUG_LOG_TDM
+    struct buffer tdm_blob = read_file("/dev/drm_dp_aux0", "rb", PANEL_TDM, 2);
+    if (tdm_blob.size < 2) {
+        fprintf(stderr, "Failed to get display tdm.\n");
         return 1;
     }
+
+    int gamma_cal_pts = 1;
+    if (is_boe) {
+        /* BOE */
+        if (tdm_blob.data.u8[0] == 0xDAu && tdm_blob.data.u8[1] == 0x14u)
+            gamma_cal_pts = 2;
+    } else {
+        /* SDC */
+        if (tdm_blob.data.u8[0] == 0x04u && tdm_blob.data.u8[1] == 0x75u)
+            gamma_cal_pts = 2;
+    }
+#endif
 
     /* Grab the mura blob from our mapped bios region. */
     struct buffer mura_blob = read_file("/dev/mem", "rb", 0xFFAA0000, MURA_BLOB_SIZE);
@@ -163,13 +202,23 @@ int main(int argc, char* argv[]) {
     printf("Success! My relief is almost palpable...\n");
     printf("Mura Blob Path: %s\n", out_file_path);
     /* Valve and Galileo already have newlines in the dmi vendor/product. Heh... */
-    printf("Vendor: %.*s", (int)vendor.size, vendor.data);
-    printf("Product: %.*s", (int)product.size, product.data);
-    printf("Display Serial: %1X%1X%1X%1X%1X\n",
-        (unsigned int)display_serial.data[0] & 0xFFu,
-        (unsigned int)display_serial.data[1] & 0xFFu,
-        (unsigned int)display_serial.data[2] & 0xFFu,
-        (unsigned int)display_serial.data[3] & 0xFFu,
-        (unsigned int)display_serial.data[4] & 0xFFu);
+    printf("Vendor: %.*s", (int)vendor.size, vendor.data.s8);
+    printf("Product: %.*s", (int)product.size, product.data.s8);
+    printf("Manufacturer: %s\n", is_boe ? "BOE" : "SDC");
+#ifdef DEBUG_LOG_TDM
+    printf("TDM: %02hhX%02hhX\n", tdm_blob.data.u8[0], tdm_blob.data.u8[1]);
+    printf("Gamma Calibration Points: %d\n", gamma_cal_pts);
+#endif
+    printf("Anti-glare: %s\n", is_antiglare == -1 ? "Unknown" : is_antiglare ? "Yes" : "No");
+    if (is_boe) {
+        printf("Display Serial: %.*s\n", 12, (const char *)display_serial.data.s8);
+    } else {
+        printf("Display Serial: %02hhX%02hhX%02hhX%02hhX%02hhX\n",
+            display_serial.data.u8[0],
+            display_serial.data.u8[1],
+            display_serial.data.u8[2],
+            display_serial.data.u8[3],
+            display_serial.data.u8[4]);
+    }
     return 0;
 }
